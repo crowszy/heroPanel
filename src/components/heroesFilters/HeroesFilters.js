fdfcb89bd20462fee03d35476e931fc9dee27b00
crowszy <danil.voronov.2013@gmail.com>
2023-07-02 17:29:57 +0300

    integrated middleware and redux-thunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -11,7 +11,7 @@ import { useHttp } from "../../hooks/http.hook";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from "../../actions";
+import { fetchFilters, activeFilterChanged } from "../../actions";
 import Spinner from "../spinner/Spinner";
 
 
@@ -22,10 +22,8 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
+        dispatch(fetchFilters(request));
+        
             // eslint-disable-next-line
     }, []);
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,34 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
 
-const store = createStore(combineReducers({heroes, filters}),
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+const store = createStore(combineReducers({heroes, filters}), compose(
+    applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 export default store;
